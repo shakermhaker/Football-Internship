740b92082cac86ebf99248f8adc7ba733e59d41a
Ömer Faruk Karaarslan <omerfaruk.karaarslann@gmail.com>
2026-06-26 11:35:36 +0300

    Swagger

---
+++ FootballField/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    internal class AutofacBusinessModule : Module
+    {
+    }
+}
--- FootballField/WebAPI/Program.cs
+++ FootballField/WebAPI/Program.cs
@@ -1,25 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// 1. .NET 10'un Kendi Servis Tanımlamaları (Başka hiçbir harici paket yok)
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); // Klasik Swagger UI üreteci
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 2. HTTP İstek Boru Hattı (Middleware Pipeline)
 if (app.Environment.IsDevelopment())
 {
+    // Proje ayağa kalktığında hata vermeden direkt Swagger UI arayüzünü açar
     app.UseSwagger();
     app.UseSwaggerUI();
-
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
