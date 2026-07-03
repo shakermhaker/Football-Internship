f368269c620ba6834662eccbe2583ff4fa332fe8
Ömer Faruk Karaarslan <omerfaruk.karaarslann@gmail.com>
2026-07-03 17:13:15 +0300

    a

---
+++ Frontend/WebUI/.angular/cache/22.0.4/WebUI/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,43 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-QKRW5UHP.js";
+import "./chunk-XVGA5YPL.js";
+import "./chunk-DWEWCYRT.js";
+import "./chunk-SNKGQ7BV.js";
+import "./chunk-UWNCB2MR.js";
+import "./chunk-L7KM3PPE.js";
+import "./chunk-POAZVYWT.js";
+import "./chunk-7VO7PY3Q.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
