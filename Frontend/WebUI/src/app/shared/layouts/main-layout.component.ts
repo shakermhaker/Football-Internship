42329331d9207bebd6654e8c96c9ee24c8239d04
Ömer Faruk Karaarslan <omerfaruk.karaarslann@gmail.com>
2026-07-03 18:11:53 +0300

    isimler

--- Frontend/WebUI/src/app/shared/layouts/main-layout.component.ts
+++ Frontend/WebUI/src/app/shared/layouts/main-layout.component.ts
@@ -9,7 +9,7 @@ import { RouterOutlet } from '@angular/router';
     <div class="main-layout-wrapper">
       <header class="p-4 shadow-sm bg-white">
         <div class="container d-flex justify-content-between align-items-center">
-          <strong style="color: #FF385C; font-size: 24px;">airbnb</strong>
+          <strong style="color: #FF385C; font-size: 24px;">FootballField</strong>
           <span class="text-muted">Profile Area</span>
         </div>
       </header>
@@ -19,7 +19,7 @@ import { RouterOutlet } from '@angular/router';
       </main>
 
       <footer class="bg-light text-center py-4 border-top">
-        <p class="mb-0 text-muted">&copy; 2026 Airbnb, Inc. All rights reserved.</p>
+        <p class="mb-0 text-muted">&copy; 2026 FootballFiedl, Inc. All rights reserved.</p>
       </footer>
     </div>
   `
