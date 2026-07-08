5b6fb8f878d0722577f25d1c608a61388c31a415
Cemre <celiendths@gmail.com>
2026-07-08 17:42:30 +0300

    just arkaplan rengi

--- Frontend/WebUI/src/app/shared/layouts/auth-layout.component.ts
+++ Frontend/WebUI/src/app/shared/layouts/auth-layout.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet], // HTML içinde <router-outlet> kullanabilmek için ekledik
   template: `
-    <div class="d-flex flex-column flex-root" style="background-color: #f7f7f7;">
+    <div class="d-flex flex-column flex-root" style="background-color: #121417;">
       <div class="d-flex flex-column flex-center min-vh-100">
         <router-outlet></router-outlet>
       </div>
