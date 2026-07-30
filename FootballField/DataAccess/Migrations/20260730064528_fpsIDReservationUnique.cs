4d2b0c52b258ebe91ca23a7168b1c62ed7323765
Ömer Faruk Karaarslan <omerfaruk.karaarslann@gmail.com>
2026-07-30 09:47:11 +0300

    migration

---
+++ FootballField/DataAccess/Migrations/20260730064528_fpsIDReservationUnique.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class fpsIDReservationUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Unique_ReservationDate_ScheduleId",
+                table: "Reservations",
+                columns: new[] { "ReservationDate", "FieldPriceScheduleId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Unique_ReservationDate_ScheduleId",
+                table: "Reservations");
+        }
+    }
+}
